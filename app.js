13573d2c9bb0dfff66c899191baf70ee6ddf9ade
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-04-05 14:09:36 +0200

    Switching classes for buttons and main sections.

---
+++ app.js
@@ -0,0 +1,27 @@
+const sections = document.querySelectorAll('.section')
+const sectionBtns = document.querySelectorAll('.controls')
+const sectionBtn = document.querySelectorAll('.control')
+const allSections = document.querySelector('.main-content')
+
+function pageTransitions(){
+
+    for (let i = 0; i < sectionBtn.length; i++){
+        sectionBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelector('.active-btn')
+            currentBtn.classList.toggle('active-btn')
+            this.classList.toggle('active-btn')
+
+            btnId = this.dataset.id
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const activeSection = document.getElementById(btnId)
+            activeSection.classList.add('active')
+
+        })
+    }
+
+}
+
+pageTransitions()
